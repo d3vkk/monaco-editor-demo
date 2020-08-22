dfc8a70aeb9e88e197d0231aca06d85c01d35e15
d3vkk <donaldkk22@yahoo.com>
2020-08-22 11:44:17 +0300

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <MonacoEditor class="editor" v-model="code" language="javascript" />
+</template>
+
+<script>
+import MonacoEditor from 'vue-monaco'
+
+export default {
+  components: {
+    MonacoEditor
+  },
+
+  data() {
+    return {
+      code: 'const noop = () => {}'
+    }
+  }
+}
+</script>
+
+<style>
+.editor {
+  width: 1600px;
+  height: 800px;
+}
+</style>
